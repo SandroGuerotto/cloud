8d28591dc226ffc37782926e8ba3118c955bbb77
SandroGuerotto <s.guerotto@gmail.com>
2016-09-26 21:54:32 +0200

    homescreen

--- src/handler/EventhandlerHomeScreen.java
+++ src/handler/EventhandlerHomeScreen.java
@@ -1,13 +1,32 @@
 package handler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
 
 /*
  * Name			: EventhandlerHomeScreen.java
  * Author		: Sandro Guerotto
  * Describtion	: Handler class for all Event from the Home Screen
  * Create on 	: 20.09.2016
- * Last modify  : dd.mm.yyyy name reason
+ * Last modify  : 26.29.2016 Sandro Erstellung
  */
+
 public class EventhandlerHomeScreen {
 
+    @FXML
+    private Label lbl_title, lbl_username, lbl_time;
+
+    @FXML
+    private Button btn_dropbox;
+
+    @FXML
+    private Hyperlink btn_logout;
+
+    public void iniinitialize(){
+
+
+
+    }
 
 }
